035a781d9516437c57331bdb3ba43a80d0a2696b
@igor90p <mmm.freedom@ya.ru>
2018-10-17 21:06:45 +0400

    Ввел решение

--- hw_04_easy.py
+++ hw_04_easy.py
@@ -9,4 +9,7 @@ __author__ =  "Пономарев Игорь Борисович"
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
 
 list_1 = [1, 2, 4, 0]
+print("list_1:", list_1)
+list_2 = list(map(lambda x:x**2, list_1))
+print("list_2:", list_2)
  
