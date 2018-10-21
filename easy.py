caf4a20ae4b2833f22c9c8eb059b90f5c84abd17
@igor90p <mmm.freedom@ya.ru>
2018-10-21 13:32:48 +0400

    вывел решение

--- easy.py
+++ easy.py
@@ -43,3 +43,10 @@ if __name__ == "__main__":
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+
+def list_dir(main_path):
+    for _ in os.listdir(main_path):
+        print(_)
+main_path = os.getcwd()
+ 
+list_dir(main_path)
