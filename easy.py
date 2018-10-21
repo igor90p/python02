6a393554f43c8b069499198a33b88bf27b08c5b0
@igor90p <mmm.freedom@ya.ru>
2018-10-21 13:30:57 +0400

    удалил директорию

--- easy.py
+++ easy.py
@@ -23,3 +23,20 @@ def new_path(name):
 if __name__ == "__main__":
     for i in range(1, 10):
                           new_path('{}_{}'.format("dir", i))
+
+                          # Удаление директорий
+def path_remove(path_to_remove):
+    ui_sure = input("{} {} {}".format("Вы уверены, что хотите удалить ", path_to_remove, "? Y/N"))
+ 
+    if ui_sure == "y" or ui_sure == "Y":
+        try:
+            os.removedirs(path_to_remove)
+            print("Вы успешно удалили " + path_to_remove)
+        except (TypeError, FileNotFoundError):
+            print("Не верно указан путь")
+    else:
+        print("Операция отменена")
+ 
+if __name__ == "__main__":
+    for i in range(1, 10):
+                          path_remove("{}_{}".format("dir", i))
