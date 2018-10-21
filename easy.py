b388dfb1f0ecd069df2b5905555ee989a431a67f
@igor90p <mmm.freedom@ya.ru>
2018-10-21 13:35:02 +0400

    вывел решение

--- easy.py
+++ easy.py
@@ -53,3 +53,29 @@ list_dir(main_path)
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+def copy_file(first_file,backup_file):
+    shutil.copy(first_file,backup_file)
+ 
+first_file = sys.argv[0]
+backup_file = first_file + ".backup"
+copy_file(first_file,backup_file)
+ 
+ 
+def delete_dir(dir_path):
+    dir_path = os.path.join(os.getcwd(), dir_path)
+    try:
+        os.rmdir(dir_path)
+    except:
+        print(dir_path + " - такой директории нет")
+
+ 
+def change_dir(dir_path):
+    try:
+        os.chdir(dir_path)
+        print(os.getcwd() + " - текущая директория")
+    except:
+        print(dir_path + " - такой директории нет")
+ 
+dir_path = "D:\\python02\\"
+change_dir(dir_path)
