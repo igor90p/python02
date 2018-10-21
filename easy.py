d76ba129e14811eb01fd07d08be8b6f43a6d3feb
@igor90p <mmm.freedom@ya.ru>
2018-10-21 13:30:16 +0400

    создал директорию

--- easy.py
+++ easy.py
@@ -9,3 +9,17 @@ import sys
  # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке, из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
+
+
+# Создание директорий
+def new_path(name):
+    path = os.path.join(name)
+    try:
+        os.makedirs(path)
+        print("Создана папка " + name)
+    except FileExistsError:
+         print("Директория с таким именем уже существует")
+          
+if __name__ == "__main__":
+    for i in range(1, 10):
+                          new_path('{}_{}'.format("dir", i))
